e108fbbcfed185fa4296f00bd58f368c1813dfb1
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-11-01 14:41:53 +0000

    Initial commit.
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@6 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ Policy/FieldSpecification.class.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Contains abstract parent class of all FieldPolicy implementations.
+ *  
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @filesource
+ */
+//namespace Phaser\Policy;
+require_once 'Policy/Specification.interface.php';
+require_once 'Policy/AndSpecification.class.php';
+require_once 'Policy/OrSpecification.class.php';
+require_once 'Policy/NotSpecification.class.php';
+/**
+ * Examine a field in a datasource
+ * 
+ * Data source should be any array or calss implementing Iterable interface.
+ * It has methods to implement logical criteria to make it easier creating 
+ * complex policies. All its subclasses will get the same methods, so if any 
+ * new logical policy class are implemented, they should be added to this 
+ * class.  
+ * 
+ * @package Phaser
+ * @subpackage Policy
+ * @abstract 
+ */
+abstract class FieldSpecification implements Specification{
+    
+    /**
+     * @var string
+     */
+    protected $field='';
+    
+    /**
+     * Stores field name for later use.
+     *
+     * @param string $field
+     */
+    public function __construct($field){
+        $this->field = $field;
+    }
+    
+    /**
+     * Shortcut method for "And" type logical policy
+     *
+     * @param Specification $spec
+     * @return AndSpecification
+     */
+    public function and_(Specification $spec){
+        return new AndSpecification($this, $spec);
+    }
+    
+    /**
+     * Shortcut method for "Or" type logical policy
+     *
+     * @param Specification $spec
+     * @return OrSpecification
+     */
+    public function or_(Specification $spec){
+        return new OrSpecification($this, $spec);
+    }
+    
+    /**
+     * Shortcut method for "Not" type logical policy
+     *
+     * @param Specification $spec
+     * @return NotSpecificationn
+     */
+    public function not_(Specification $spec){
+        return new NotSpecification($spec);
+    }
+}
+?>
