6d8ee4bf0e9673c9f9382c9c43a9b99590d97dae
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-11-16 15:43:00 +0000

    Initial commit.
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@17 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ Server/Daemon.interface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Contains definition of Daemon Interface
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @filesource
+ */
+/**
+ * Daemon Interface definition for creating processes running on their 
+ * own as daemons.
+ * 
+ * @package Phaser
+ * 
+ */
+interface Daemon{
+    public function summon();
+    public function spawn();
+    public function expell();
+    public function setMaxSpawns($num);
+}
+?>
