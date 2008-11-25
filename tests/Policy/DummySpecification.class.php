b08687a30b7c0c103bd787ae5bc9ae410bc8d19e
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-11-25 22:15:46 +0000

    Dummy policy plugin class for testing purposis
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@38 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ tests/Policy/DummySpecification.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File documentation
+ *
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @copyright Copyright (c) 2008, Peter Nagy
+ * @filesource
+ */
+/**
+ * Class documentation
+ */
+class DummySpecification{
+
+}
+?>
