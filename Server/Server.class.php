43bf5c776bd062f5bc83fba60ffcbcf25c296a8c
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-12-20 15:17:24 +0000

    +Stub for IPC communication
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@45 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Server/Server.class.php
+++ Server/Server.class.php
@@ -1,110 +1,136 @@
 <?php
 /**
  * Contains main server implementation
- * 
+ *
  * @author Peter Nagy <antronin@gmail.com>
  * @version $Id$
  * @copyright Copyright (c) 2008, Peter Nagy
+ * @package Server
  * @filesource
  */
 /***/
 require_once 'Server/Daemon.interface.php';
+require_once 'Comm/Ipc/IpcFactory.class.php';
 /**
  * Implementation of Daemon interface
- * 
+ *
  * Couples all required responsibilities to create a Server process
- * 
+ *
  * @package Server
  */
 abstract class Server implements Daemon{
     private $maxSpawns = 5;
+    protected $role = 'parent';
     protected $spawns = array();
     protected $pidFile = null;
     protected $pidFolder = null;
     protected $pid;
-    
-    public function __construct(){
+    protected $ipc = null;
+    protected $ipcType = '';
+
+    public function __construct($ipcType = '')
+    {
         ini_set('max_execution_time',0);
         ini_set('max_input_time',0);
         set_time_limit(0);
+        $this->ipcType = $ipcType;
+        $this->pid = getmypid();
     }
-    
-    final public function summon(){
-        $this->pidFile = fopen($this->pidFolder.'/.'.$this->appID.'.pid', "a");
-        if (!$this->pidFile || !flock($this->pidFile, LOCK_EX | LOCK_NB))
-        {
-            fputs(STDERR, "Failed to acquire lock\n");
-            exit;
-        }
-        $pid = pcntl_fork();
-        if ($pid < 0)
-        {
-            return false;
-        }
-        elseif($pid==0)
-        {//child process go here
-            
-            posix_setsid();
-            $this->spawn();
-            fwrite($this->pidFile, $this->pid);
-            fflush($this->pidFile);
+
+    final public function summon()
+    {
+        $this->spawn();
+        if($this->role == 'child')
+        {//we are the new process
+            $this->savePid2File();
             pcntl_signal(SIGCHLD, array($this,'signalHandler'),true);
             pcntl_signal(SIGUSR1, array($this,'signalHandler'),true);
+            //changing back as from now on we are the Parent server process
+            $this->role == 'parent';
+            fputs(STDOUT, 'Server process (pid:'.$this->pid.") summoned\n");
             $this->startHart();
         }
-        else 
+        else
         {//we are the parent process
             return true;
         }
     }
-    
-    protected function startHart(){
+
+    private function savePid2File()
+    {
+        $this->pidFile = fopen($this->pidFolder.'/.phaser'.$this->appID.'.pid', "a");
+        if (!$this->pidFile || !flock($this->pidFile, LOCK_EX | LOCK_NB))
+        {
+            throw new Exception('Unable to get pid file lock!');
+        }
+        fwrite($this->pidFile, $this->pid);
+        fflush($this->pidFile);
+    }
+
+    protected function startHart()
+    {
         while(true){
             usleep(500);
         }
     }
-    
-    public function spawn(){
+
+    public function spawn()
+    {
         if (count($this->spawns) < $this->maxSpawns)
         {
-            if($pid = pcntl_fork())
+            $pid = pcntl_fork();
+            if($pid < 0)
             {
+                throw new Exception('Unable to fork!');
+            }elseif($pid == 0){
                 $this->pid = getmypid();
-                $this->spawns[] = $pid; 
-                //fputs(STDOUT, var_dump($this));
+                $this->role = 'child';
+                $this->ipc = ($this->ipcType !== '')?IpcFactory::get($ipcType,$this->pid):null;
+            }else{
+                $this->spawns[] = $pid;
             }
-            /*else
-            {
-                exit;
-            }*/
+            fputs(STDOUT, $pid." spawned\n");
         }
+
     }
-    
-    public function expell(){
-        fputs(STDOUT, 'shuting down: '.$this->appID."\n");
+
+    public function expell()
+    {
+        fputs(STDOUT, "children: \n");
+        $success = false;
+        foreach($this->spawns as $child)
+        {
+            fputs(STDOUT, "$child \n");
+            posix_kill ($child, SIGSTOP);
+            pcntl_waitpid ($child, $temp = 0, WNOHANG);
+            $success = pcntl_wifexited ($temp);
+        }
+        return $success;
         fclose($this->pidFile);
-        unlink($this->pidFolder.'/.'.$this->appID.'.pid');
+        unlink($this->pidFolder.'/.phaser'.$this->appID.'.pid');
     }
-    
-    public function setMaxSpawns($num){
+
+    public function setMaxSpawns($num)
+    {
         $this->maxSpawns = $num;
     }
-    
-    private function signalHandler($sigCode){
+
+    private function signalHandler($sigCode)
+    {
         switch($sigCode){
             case 0:
                 return;
             break;
             case SIGCHILD:
+                pcntl_signal(SIGCHLD, array($this,'signalHandler'),true);
                 fputs(STDOUT, 'signal received:'.$this->lastSignal);
                 fputs(STDOUT, ' means SIGCHILD...');
-                pcntl_signal(SIGCHLD, array($this,'signalHandler'),true);
                 $this->expell();
             break;
             case SIGUSR1:
+                pcntl_signal(SIGUSR1, array($this,'signalHandler'),true);
                 fputs(STDOUT, 'signal received:'.$this->lastSignal);
                 fputs(STDOUT, ' means SIGUSR1...');
-                pcntl_signal(SIGUSR1, array($this,'signalHandler'),true);
                 $this->expell();
             break;
             default:
