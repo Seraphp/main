fbda8c11ca3914bb1153f8478329537bf4486799
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-12-20 20:55:38 +0000

    IpcFactory implemented
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@48 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Comm/Ipc/Ipc.interface.php
+++ Comm/Ipc/Ipc.interface.php
@@ -1,14 +1,18 @@
 <?php
 /**
- * File documentation
+ * Holds IPC interface definition
  *
  * @author Peter Nagy <antronin@gmail.com>
  * @version $Id$
  * @copyright Copyright (c) 2008, Peter Nagy
+ * @package Comm
+ * @subpackage Ipc
  * @filesource
  */
 /**
- * Class documentation
+ * IPC interface definition
+ * @package Comm
+ * @subpackage Ipc
  */
 interface Ipc{
 
--- Comm/Ipc/IpcFactory.class.php
+++ Comm/Ipc/IpcFactory.class.php
@@ -1,29 +1,77 @@
 <?php
 /**
- * File documentation
+ * Holds IpcFactory class implementation
  *
  * @author Peter Nagy <antronin@gmail.com>
  * @version $Id$
  * @copyright Copyright (c) 2008, Peter Nagy
+ * @package Comm
+ * @subpackage Ipc
  * @filesource
  */
 /***/
 require_once 'Comm/Ipc/Ipc.interface.php';
+require_once 'Exceptions/PluginException.class.php';
 /**
- * Class documentation
+ * Static class to retrieve Ipc implementation classes
+ *
+ * Retreive already initalized InterProcess Communication imlementation
+ * classes, depending on the given type. Class checks if the requested
+ * implementation type is exists in the filesystem. Requesting such not
+ * implemented types results throwing Exception.
  */
 class IpcFactory{
 
+    private static $pluginsDir  = '';
     /**
      * Returns a object which implements Ipc interface
      *
      * @param string $type
      * @param integer $pid
      * @return Ipc
+     * @throws PluginException
      * @static
      */
-    static function get($type,$pid){
-        return;
+    static function get($type,$pid)
+    {
+        if(self::isValidIpc($type))
+        {
+            $className = self::getClassName($type);
+            $class = new $className;
+            $class->init($pid);
+            return $class;
+        }
+    }
+
+    static function getClassName($type)
+    {
+        return sprintf('Ipc%s',ucfirst($type));
+    }
+
+    /**
+     * Checks if given type has implementation class
+     *
+     * Classes has to be named as Ipc<Type>.class.php to make IpcFactory
+     * recognize them.
+     * For example:
+     *   IpcPipe.class.php will be recognized as "pipe".
+     *
+     * @param string $plugin
+     * @throws PluginException
+     */
+    private static function isValidIpc($type)
+    {
+        $pluginFile = sprintf('%s/%s.class.php',$this->pluginsDir,self::getClassName($type));
+        if(is_file($pluginFile))
+        {
+            require_once $pluginFile;
+            $class = new ReflectionClass(self::getClassName($type));
+            if($class->implementsInterface('Ipc'))
+            {
+                return true;
+            }
+            else throw new PluginException('Ipc interface not implemented in '.$type);
+        }
     }
 }
 ?>
