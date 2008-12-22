0dc0021f4546200c6e4f95e7a2e9293d68ad1289
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-12-22 14:54:28 +0000

    + Exceptions moved to new package
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@52 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ Exceptions/PluginException.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Holds  PluginException implementation
+ *
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @copyright Copyright (c) 2008, Peter Nagy
+ * @package Exceptions
+ * @filesource
+ */
+/**
+ * Exception class for policy plugin related issues
+ *
+ * @package Exceptions
+ */
+class PluginException extends Exception{}
+?>
