f2aecb36730ba2f102f5e2fa5ec80e58cf86afe9
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-12-30 21:33:22 +0000

    Initital commit of Registry test
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@58 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ tests/Server/Registry/AppServerRegistryTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File documentation
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @filesource
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'Server/Registry/AppServerRegistry.class.php';
+require_once 'Server/AppServer.class.php';
+/**
+ * Class documentation
+ */
+class AppServerRegistryTest extends PHPUnit_Framework_TestCase{
+
+    private $reg = null;
+
+    function setUp()
+    {
+        $this->reg = AppServerRegistry::getInstance();
+        $this->mockServer = $this->getMock('AppServer', array('getStatus'),array('mockery'));
+    }
+
+    function testRegistryIsSingleton()
+    {
+        $this->assertThat($this->reg, $this->IsInstanceOf('AppServerRegistry'));
+        $this->assertSame($this->reg, AppServerRegistry::getInstance());
+    }
+
+    function testGetNonRunningServerStatus()
+    {
+        $this->assertNull($this->reg->getAppStatus('something'));
+    }
+
+    function testGetNonRunningServerInstance()
+    {
+        $this->assertNull($this->reg->getAppInstance('something'));
+    }
+
+    function testAddValidApp()
+    {
+        $this->mockServer->expects($this->once())->method('getStatus');
+        $this->assertTrue($this->reg->addApp('mockery',$this->mockServer));
+        $this->assertEquals($this->reg->getAppStatus('mockery'),null);
+        $this->assertSame($this->reg->getAppInstance('mockery'),$this->mockServer);
+        $this->setExpectedException('RegistryException');
+        $this->assertTrue($this->reg->addApp('mockery',$this->mockServer));
+        $this->assertSame($this->reg->removeApp('mockery'), $this->mockServer);
+        $this->setExpectedException('RegistryException');
+        $this->reg->removeApp('mockery');
+    }
+
+}
+?>
