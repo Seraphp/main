7ab16c3c113ffc7c5625dab0c1b7db30d7416c70
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-12-31 20:08:49 +0000

    Initial commit
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@73 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ tests/Comm/Ipc/AllTests.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File documentation
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @filesource
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'IpcFactoryTest.php';
+require_once 'IpcUnixsocketsTest.php';
+/**
+ * Class documentation
+ */
+class Ipc_AllTests{
+
+    public static function suite()
+    {
+        // @codeCoverageIgnoreStart
+        $suite = new PHPUnit_Framework_TestSuite('Ipc facilities');
+        $suite->addTestSuite('IpcFactoryTest');
+        $suite->addTestSuite('IpcUnixsocketsTest');
+        return $suite;
+        // @codeCoverageIgnoreEnd
+    }
+}
+?>
---
+++ tests/Comm/Ipc/IpcUnixsocketsTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * File documentation
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @filesource
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'Comm/Ipc/IpcUnixsockets.class.php';
+/**
+ * Class documentation
+ */
+class IpcUnixsocketsTest extends PHPUnit_Framework_TestCase{
+
+    function setUp()
+    {
+        $this->msg = 'Árvíztűrő tükörfúrógép';
+        $this->ipc = new IpcUnixsockets();
+        $this->ipc->init(1234,'child');
+    }
+
+    function testInit()
+    {
+        $this->assertEquals('child',$this->ipc->init(1234,'child'));
+    }
+
+    function testRoleSettings()
+    {
+        $this->assertEquals('child',$this->ipc->getRole());
+        $this->assertEquals('parent',$this->ipc->setRole('parent'));
+        $this->assertEquals('parent',$this->ipc->setRole('test'));
+    }
+
+    function testWrite()
+    {
+        $this->assertEquals(strlen($this->msg)+1,$this->ipc->write(1234, $this->msg));
+    }
+
+    function testRead()
+    {
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+        $this->assertEquals($this->msg,$this->ipc->read());
+    }
+
+    function testClose()
+    {
+        $this->ipc->close();
+    }
+
+    function testDestructor()
+    {
+        unset($this->ipc);
+    }
+}
+?>
