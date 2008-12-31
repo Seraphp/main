bb30f2fef82bf614443e0b831bc0b34a05d5858d
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2008-12-31 20:33:25 +0000

    + Added Config test suit + Added Datstore test file
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@78 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- tests/Server/AllTests.php
+++ tests/Server/AllTests.php
@@ -6,8 +6,10 @@
  * @filesource
  */
 require_once 'PHPUnit/Framework.php';
-require_once 'tests/Server/AppServerTest.php';
-require_once 'tests/Server/Registry/AllTests.php';
+require_once 'Server/AppServerTest.php';
+require_once 'DataStoreTest.php';
+require_once 'Registry/AllTests.php';
+require_once 'Config/AllTests.php';
 /**
  * Class documentation
  */
@@ -18,7 +20,9 @@ class Server_AllTests{
         // @codeCoverageIgnoreStart
         $suite = new PHPUnit_Framework_TestSuite('Phaser Server');
         $suite->addTestSuite('AppServerTest');
+        $suite->addTestSuite('DataStoreTest');
         $suite->addTest(Registry_AllTests::suite());
+        $suite->addTest(Config_AllTests::suite());
         return $suite;
         // @codeCoverageIgnoreEnd
     }
