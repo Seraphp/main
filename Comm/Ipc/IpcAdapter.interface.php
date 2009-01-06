551cc25ee12aab5cdc7e9a9c00a7f24acacf6c7b
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-01-06 19:02:13 +0000

    Ipc interface renamed to IpcAdapter
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@98 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Comm/Ipc/IpcAdapter.interface.php
+++ Comm/Ipc/IpcAdapter.interface.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * Holds IPC interface definition
+ * Holds IpcAdapter interface definition
  *
  * @author Peter Nagy <antronin@gmail.com>
- * @version $Id$
+ * @version $Id:IpcAdapter.interface.php 321 2009-01-06 18:44:47Z peter $
  * @copyright Copyright (c) 2008, Peter Nagy
  * @package Comm
  * @subpackage Ipc
  * @filesource
  */
 /**
- * IPC interface definition
+ * IpcAdapter interface definition
  * @package Comm
  * @subpackage Ipc
  */
-interface Ipc{
+interface IpcAdapter{
 
     function init($pid, $role);
     function read();
