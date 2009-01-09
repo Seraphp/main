25dea3b03c6cfd0d8e74d110db6e9d44b405a942
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-01-09 20:14:33 +0000

    Added an additional getter MEthod for MaxSpawns on the interface.
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@117 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Server/Daemon.interface.php
+++ Server/Daemon.interface.php
@@ -8,16 +8,17 @@
  * @filesource
  */
 /**
- * Daemon Interface definition for creating processes running on their 
- * own as daemons.
- * 
+ * Daemon Interface definition for creating processes running on their
+ * own (as a daemon in UNIX terminology).
+ *
  * @package Server
- * 
+ *
  */
 interface Daemon{
     public function summon();
     public function spawn();
     public function expell();
     public function setMaxSpawns($num);
+    public function getMaxSpawns();
 }
 ?>
