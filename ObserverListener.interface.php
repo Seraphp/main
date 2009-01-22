2fe576271c95a7674977bd54bed630ed7c6347b3
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-01-22 19:51:49 +0000

    Added documentation
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@122 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- ObserverListener.interface.php
+++ ObserverListener.interface.php
@@ -8,28 +8,69 @@
  */
 /**
  * Observer interface definition
- * 
- * Classes implementing this interface will be able to receive 
+ *
+ * Classes implementing this interface will be able to receive
  * notification from classes implementing the Listener interface.
- * 
+ *
  * @package Phaser
  */
 interface Observer{
+    /**
+     * Returns a unique name of the Observer instance
+     *
+     * @return string
+     */
     public function getName();
+    /**
+     * Notification method
+     *
+     * Calling this method will mean, that the Listener's status has been
+     * changed.
+     *
+     * @param Listener $instance
+     * @return void
+     */
     public function update(Listener $instance);
 }
 /**
  * Listener interface definition
- * 
- * Classes implementing this interface will be able to  
+ *
+ * Classes implementing this interface will be able to
  * notify classes implementing the Observer interface.
- * 
+ *
  * @package Phaser
  */
 interface Listener{
+    /**
+     * Attaches an observer
+     *
+     * The instance will receives the update notifications.
+     *
+     * @param Observer $instance
+     * @return boolean
+     */
     public function attach(Observer $instance);
+    /**
+     * Notifies observers
+     *
+     * Flicks through all registered observers and notify them about update
+     * @return void
+     */
     public function notify();
+     /**
+     * Return observed status report
+     *
+     * @return mixed
+     */
     public function getState();
+    /**
+     * Deatches an observer
+     *
+     * The  instance will not receive updates anymore.
+     *
+     * @param Observer $instance
+     * @return boolean
+     */
     public function detach(Observer $instance);
 }
 ?>
