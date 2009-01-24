3db731f92b411b1e0e8d87fb0954ba8a384771ce
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-01-24 12:20:02 +0000

    Preparation for JSON-RPC based interserver communication
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@125 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ Comm/JsonRpc/Request.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Hold implementation of JSON-RPC REquest class
+ *
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @copyright Copyright (c) 2009, Peter Nagy
+ * @package Comm
+ * @subpackage JsonRpc
+ * @filesource
+ */
+/***/
+require_once 'Comm/Request.interface.class';
+/**
+ * Implements simple JSON-RPC Request class
+ *
+ * @package Comm
+ * @subpackage JsonRpc
+ * @since JSON-RPC 1.0
+ */
+class Request implements Request{
+     public $id = null;
+     public $method = '';
+     public $params = array();
+
+     public function __construct($method, $params = array(), $id = null)
+     {
+         if( empty( $method ) )
+         {
+             throw new Exception('Method has to be defined!');
+         }
+         else
+         {
+             $this->method = $method;
+             $this->params = $params;
+             $this->id = $id;
+         }
+     }
+
+     public function __toString()
+     {
+         return json_encode($this);
+     }
+}
+?>
---
+++ Comm/JsonRpc/Response.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Hold implementation of JSON-RPC Response class
+ *
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @copyright Copyright (c) 2009, Peter Nagy
+ * @package Comm
+ * @subpackage JsonRpc
+ * @filesource
+ */
+/**
+ * Implements simple JSON-RPC Response class
+ *
+ * @package Comm
+ * @subpackage JsonRpc
+ * @since JSON-RPC 1.0
+ */
+class Response{
+    public $result = null;
+    public $error = null;
+    public $id = null;
+
+    public function __construct($result, $error = null, $id = null)
+    {
+        if ( $result === null && $error === null )
+        {
+        	throw new Exception('Either a result or an error should be set!');
+        }
+        else
+        {
+            $this->error = $error;
+          	$this->result = $result;
+          	$this->id = $id;
+        }
+    }
+
+    public function __toString()
+    {
+        return json_encode($this);
+    }
+}
+?>
