4cb92dfaa1446783f7d4aa55d796aacb6fc310ff
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-01-26 21:51:48 +0000

    CLOSED - # 2: Add HTTP connection https://customer-251.radio-net.hu/projects/phaser/ticket/2
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/phaser/trunk@128 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Comm/Http/HttpCookie.class.php
+++ Comm/Http/HttpCookie.class.php
@@ -1,17 +1,47 @@
 <?php
 /**
- * File documentation
+ * Contains the class HTTPCookie for representing a web cookie
  *
  * @author Peter Nagy <antronin@gmail.com>
  * @version $Id$
  * @copyright Copyright (c) 2009, Peter Nagy
+ * @package Comm
+ * @subpackage Http
  * @filesource
  */
 /**
- * Class documentation
+ * Class representing an HTTP cookie
+ * @package Comm
+ * @subpackage Http
  * @todo Implement HttpCookie class
  */
 class HttpCookie{
+    public $name = '';
+    public $value = null;
+    public $expireOn = null;
+    public $path = null;
+    public $domain = null;
+    public $secure = false;
+    public $onlyHTTP = false;
 
+    public function __construct($name, $value=false, $expireOn = 0, $path='/', $domain='null', $secure = false, $onlyHTTP=false)
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->expireOn = $expireOn;
+        $this->path = $path;
+        $this->domain = $domain;
+        $this->secure = $secure;
+        $this->onlyHTTP = $onlyHTTP;
+    }
+
+    public function __toString()
+    {
+        ob_start();
+        setcookie($this->name, $this->value, $this->expireOn, $this->path, $this->domain, $this->onlyHTTP);
+        $cookieStr = ob_get_contents();
+        ob_end_clean();
+        return $cookieStr;
+    }
 }
 ?>
