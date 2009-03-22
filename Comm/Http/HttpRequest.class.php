1014bd17e08d4f6c5f4f27836bb3a57b46d8ddc9
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-03-22 20:41:19 +0000

    Making files Zend coding-style compatible
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@164 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Comm/Http/HttpRequest.class.php
+++ Comm/Http/HttpRequest.class.php
@@ -107,7 +107,7 @@ class HttpRequest implements Request, Listener
      */
     public function __construct( $sock = null )
     {
-        if ( $sock !== null && is_resource( $sock ) ) {
+        if ( $sock !== null && is_resource($sock) ) {
          //If parameter is a resource, it means the class will represent a
          //received request
             $this->_socket = $sock;
@@ -121,7 +121,7 @@ class HttpRequest implements Request, Listener
     protected function _parse()
     {
         if ( $this->isReceived === self::REQ_RECEIVED ) {
-           while ( $this->_buffer = @socket_read( $this->_socket, 512 ) ) {
+           while ( $this->_buffer = @socket_read($this->_socket, 512) ) {
                $this->_save();
                if ( $this->httpHeaders !== array() ) {
                //We notify only when all headers are received
@@ -150,12 +150,10 @@ class HttpRequest implements Request, Listener
              strpos( $this->message, "\r\n\r\n") !== false ) {
                 //if received first time a datachunk containing an empty line,
                 //we arrived at the end of the http header
-            list(
-                $this->httpRawHeaders,
-                $this->message
-            ) = preg_split( '/(\r\n){2}/m', $this->message );
-            $this->httpRawHeaders = trim( $this->httpRawHeaders );
-            $this->httpHeaders = explode( "\r\n", $this->httpRawHeaders );
+            list($this->httpRawHeaders, $this->message) =
+                preg_split('/(\r\n){2}/m', $this->message);
+            $this->httpRawHeaders = trim($this->httpRawHeaders);
+            $this->httpHeaders = explode("\r\n", $this->httpRawHeaders);
             $this->_processHeaders();
         }
     }
@@ -169,29 +167,26 @@ class HttpRequest implements Request, Listener
     {
         $headers = array();
         $requestLine = array_shift( $this->httpHeaders );
-        list (
-            $this->method,
-            $this->url,
-            $this->httpVersion
-        ) = explode( ' ', $requestLine );
-        $this->httpVersion = substr( $this->httpVersion, -3 );
+        list ($this->method, $this->url, $this->httpVersion) =
+            explode(' ', $requestLine);
+        $this->httpVersion = substr($this->httpVersion, -3);
         foreach ( $this->httpHeaders as $row ) {
-            $item = explode ( ':', $row, 2);
-            if ( array_key_exists( $item[0], $headers ) ) {
-                $headers[ $item[0] ] .= ';'.trim($item[1]);
+            $item = explode (':', $row, 2);
+            if ( array_key_exists($item[0], $headers) ) {
+                $headers[$item[0]] .= ';'.trim($item[1]);
             } else {
-                $headers[ $item[0] ] = trim($item[1]);
+                $headers[$item[0]] = trim($item[1]);
             }
         }
         $this->httpHeaders = $headers;
         //set up get, cookies (post will be in the body)
-        if ( array_key_exists( 'Cookie', $this->httpHeaders ) ) {
-            $this->cookies = explode( ';', $this->httpHeaders[ 'Cookies' ] );
+        if ( array_key_exists('Cookie', $this->httpHeaders) ) {
+            $this->cookies = explode(';', $this->httpHeaders['Cookies']);
             $this->cookies = HttpFactory::getCookies($this->cookies);
         }
         //processing GET parameters
-        if ( $pos = strpos( $this->url, '?' ) ) {
-        	$this->getParams = $this->params2array( substr($this->url, $pos+1));
+        if ( $pos = strpos($this->url, '?') ) {
+            $this->getParams = $this->_params2array(substr($this->url, $pos+1));
         }
         //Setting Referer
         if (array_key_exists('Referer', $this->httpHeaders) ) {
@@ -207,12 +202,12 @@ class HttpRequest implements Request, Listener
      */
     private function _params2array($str)
     {
-        $rows = explode( '&', urldecode( $str ) );
+        $rows = explode('&', urldecode($str));
         $params = array();
         $itemNum = count($rows);
         for( $idx = 0; $idx < $itemNum; $idx++ ) {
             $param = explode( '=', $rows[ $idx ], 2 );
-            unset( $rows[ $idx ] );
+            unset($rows[ $idx ]);
             $params[ $param[0] ] = $param[1];
         }
         return $params;
@@ -222,50 +217,51 @@ class HttpRequest implements Request, Listener
      * Parses hash array into urlencoded string
      *
      * @param array $params  Parameters to encode
-     * @param string $parentKey  (optional) Key to use to encode an array (deafult NULL)
-     * @param string $sep  (optional) Separator string to pőut between encoded keys (default '&')
+     * @param string $parentKey  (optional) Key to use to encode an
+     * array (deafult NULL)
+     * @param string $sep  (optional) Separator string to pőut between encoded
+     * keys (default '&')
      * @return string
      */
     private function _array2params($params, $parentKey = null, $sep = '&')
     {
         $items = array();
         foreach ( $params as $key => $value ) {
-        	if ( is_array( $value ) ) {
-        	    array_push( $items, $this->_array2params($value, $key, $sep) );
-        	} else {
-        	    if ( $parentKey !== null ) {
-        	        array_push(
-        	            $items,
-        	            sprintf( '%s[%s]=%s', $parentKey, $key, $value )
-        	        );
-        	    } else {
-        	        array_push( $items, sprintf( '%s=%s', $key, $value ));
-        	    }
-        	}
+            if ( is_array($value) ) {
+                array_push($items, $this->_array2params($value, $key, $sep));
+            } else {
+                if ( $parentKey !== null ) {
+                    array_push($items,
+                           sprintf('%s[%s]=%s', $parentKey, $key, $value));
+                } else {
+                    array_push($items, sprintf('%s=%s', $key, $value));
+                }
+            }
         }
-        return implode( $sep, $items );
+        return implode($sep, $items);
     }
 
 
     /**
-     * @return real  Ratio of the already received content against the full content length.
+     * @return real  Ratio of the already received content against the full
+     * content length.
      */
     public function getState()
     {
-        return $this->fullLength / count( $this->message );
+        return $this->fullLength / count($this->message);
     }
 
     public function notify()
     {
         foreach ( $this->_observers as $observer ) {
-            $observer->update( $this );
+            $observer->update($this);
         }
     }
 
     public function attach( Observer $observer )
     {
-        if ( !array_key_exists( $observer->getName() ) ) {
-            return $this->_observers[ $observer->getName() ] = $observer;
+        if ( !array_key_exists($observer->getName()) ) {
+            return $this->_observers[$observer->getName()] = $observer;
         } else {
             return false;
         }
@@ -274,7 +270,7 @@ class HttpRequest implements Request, Listener
     public function detach( Observer $observer )
     {
         if ( array_key_exists($observer->getName()) ) {
-            unset ( $this->_observers[ $observer->getName() ] );
+            unset($this->_observers[$observer->getName()]);
             return true;
         } else {
             return false;
@@ -289,13 +285,13 @@ class HttpRequest implements Request, Listener
     {
         //@todo Implement HttpRequest sending
         if ( $this->isReceived === self::REQ_TOSEND ) {
-            $curlObj = curl_init( $this->url );
+            $curlObj = curl_init($this->url);
             $options = array(
                 CURLOPT_CUSTOMREQUEST => ( empty( $this->method) )?'GET':$this->method,
                 CURLOPT_RETURNTRANSFER => true,
             );
             //Setting HTTP version for Curl
-            switch( $this->httpVersion ) {
+            switch ($this->httpVersion) {
                 case '1.0':
                     $options[CURLOPT_HTTP_VERSION] = CURL_HTTP_VERSION_1_0;
                     break;
@@ -307,31 +303,30 @@ class HttpRequest implements Request, Listener
                     break;
             }
             //Setting Cookies for curl
-            if ($this->cookies !== array() ) {
-            	$setCookieStr = '';
-                foreach( $this->cookies as $cookie ) {
-            	    $setCookieStr .= $cookie->__toString().';';
-            	}
-            	$options[CURLOPT_COOKIE] = $setCookieStr;
-            	unset($setCookieStr);
+            if ($this->cookies !== array()) {
+                $setCookieStr = '';
+                foreach ( $this->cookies as $cookie ) {
+                    $setCookieStr .= $cookie->__toString().';';
+                }
+                $options[CURLOPT_COOKIE] = $setCookieStr;
+                unset($setCookieStr);
             }
             //Setting up POST parameters for curl
             if ($this->postParams !== array() ) {
-            	$options[CURLOPT_POSTFIELDS] = $this->_array2params(
-            	                                   $this->postParams
-            	                               );
+                $options[CURLOPT_POSTFIELDS] =
+                   $this->_array2params($this->postParams);
             }
             //Setting up GET parameters for curl
             if ($this->getParams !== array() ) {
-            	if ( strpos( $this->url, '?' ) !== false ) {
-            		$options[CURLOPT_URL] = $this->url .
-            		     urlencode( '&'.$this->_array2params($this->getParams));
-            	} else {
-            	    $options[CURLOPT_URL] = $this->url .
-            	        '?'. urlencode( $this->_array2params($this->getParams));
-            	}
+                if ( strpos($this->url, '?') !== false ) {
+                    $options[CURLOPT_URL] = $this->url .
+                     urlencode('&'.$this->_array2params($this->getParams));
+                } else {
+                   $options[CURLOPT_URL] = $this->url .
+                    '?'. urlencode($this->_array2params($this->getParams));
+                }
             }
-            curl_setopt_array( $curlObj, $options);
+            curl_setopt_array($curlObj, $options);
             $response = curl_exec($curlObj);
             curl_close($curlObj);
             return $response;
