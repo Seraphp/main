752e8c1431b5f54ed98c4ef616ef5fe36aa51c4c
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-03-30 20:33:16 +0000

    Correct xpath error by removing namespaces Possible solution is to register xsd source domain
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@191 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- tests/Server/Config/ConfigFactoryTest.php
+++ tests/Server/Config/ConfigFactoryTest.php
@@ -35,7 +35,7 @@ class ConfigFactoryTest extends PHPUnit_Framework_TestCase{
         $conf = $this->cf->getConf('main');
         $this->assertThat($conf,$this->IsInstanceOf('Config'));
         $this->assertEquals('/home/peter/workspace/seraphp',$conf->pidpath);
-        $this->assertEquals(5,$conf->instance['maxchildren']);
+        $this->assertEquals(5,$conf->instance['maxChildren']);
         $this->assertEquals('127.0.0.1',$conf->instance['address']);
         $this->assertEquals(8123,$conf->instance['port']);
         $this->assertType('array',$conf->urimap);
@@ -48,7 +48,7 @@ class ConfigFactoryTest extends PHPUnit_Framework_TestCase{
         $conf = $this->cf->getConf('session');
         $this->assertThat($conf,$this->IsInstanceOf('Config'));
         $this->assertEquals('/home/peter/workspace/seraphp',$conf->pidpath);
-        $this->assertEquals(5,$conf->instance['maxchildren']);
+        $this->assertEquals(5,$conf->instance['maxChildren']);
         $this->assertEquals('127.0.0.1',$conf->instance['address']);
         $this->assertEquals(8124,$conf->instance['port']);
         $this->assertNull($conf->urimap);
@@ -61,7 +61,7 @@ class ConfigFactoryTest extends PHPUnit_Framework_TestCase{
         $conf = $this->cf->getConf('dbpool');
         $this->assertThat($conf,$this->IsInstanceOf('Config'));
         $this->assertEquals('/home/peter/workspace/seraphp',$conf->pidpath);
-        $this->assertEquals(5,$conf->instance['maxchildren']);
+        $this->assertEquals(5,$conf->instance['maxChildren']);
         $this->assertEquals('127.0.0.1',$conf->instance['address']);
         $this->assertEquals(8125,$conf->instance['port']);
         $this->assertNull($conf->urimap);
