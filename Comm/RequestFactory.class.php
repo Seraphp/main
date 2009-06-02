22c247ba9e3257656a3cd5d2a56e1f4827c74723
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-06-02 21:47:10 +0000

    Using stream_* functions and select from now on
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@240 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Comm/RequestFactory.class.php
+++ Comm/RequestFactory.class.php
@@ -10,6 +10,7 @@
  */
 /***/
 //namespace Seraphp\Comm;
+require_once 'Exceptions/IOException.class.php';
 /**
  * Creates Request object based on socket load's first 100 byte
  *
@@ -29,32 +30,42 @@ class RequestFactory
      * Returns an object reference which is implements Request interface
      *
      * @return Request
+     * @throws IOException  If no data in socket arrived
      */
     public static function create($socket)
     {
+        $read = array($socket);
+        while (stream_select($read, $write = null, $except = null, 0, 10) < 1) {
+            //todo: implement listening timout here as a multiplier of usleep value
+            //usleep(10);
+        }
         //using socket_recv with MSF_PEEK to examin the first part
         //of the message without removing it form the socket.
-        $result = socket_recv($socket, $peek, 100, MSG_PEEK);
-        if ( $result !== false ) {
-            if ($peek !== null) {
-                if ( $result == 0 ) {
+        $result = stream_socket_recvfrom($socket, 300, STREAM_PEEK);
+        var_dump($result);
+//        if ( $result !== false ) {
+            if ($result !== null) {
+                if (empty($result)) {
                     throw new IOException('No data on line!');
                 } else {
-                    switch( self::getProtocol($peek) ) {
+                    switch( self::getProtocol($result) ) {
                         case 'http':
                             require_once 'Comm/Http/HttpFactory.class.php';
                             return HttpFactory::create('request', $socket);
                             break;
                     }
                 }
+            } else {
+                throw new IOException('No data on line!');
             }
-        } else {
+ /*       } else {
             if ( $result === false) {
                 throw new IOException('Connection reset by peer!');
             } else {
                 throw new IOException('Connection gracefully closed by peer!');
             }
         }
+*/
     }
 
     /**
