a192c82ec31e7a69301350507246d36a2e060ae8
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-06-21 18:00:18 +0000

    Static file server engine implementation
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@268 5e58f187-e6ed-c349-b56f-493b30cdff1d

---
+++ Server/StaticFileServerEngine.class.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Holds StaticFileServerEngine implementation class
+ *
+ * @author Peter Nagy <antronin@gmail.com>
+ * @version $Id$
+ * @copyright Copyright (c) 2009, Peter Nagy
+ * @package Server
+ * @filesource
+ */
+/***/
+require_once 'Server/AppEngine.interface.php';
+/**
+ * StaticFileServerEngine class
+ *
+ * This class serves static files as is for when they are requested.
+ * It identify the file requested reads it and send it out with the correct
+ * http response.
+ *
+ * @package Server
+ * @todo Implement Request handling
+ */
+class StaticFileServerEngine implements AppEngine
+{
+    private static $_log;
+    protected $_basePath = '';
+
+    public function __construct(Config $conf)
+    {
+        self::$_log = LogFactory::getInstance($conf->server);
+        self::$_log->debug(__METHOD__.' called');
+        $param = $conf->xsearch('srph:param[@name="basepath"]');
+        if ($param !== false) {
+            $this->_basePath = (string)$param[0];
+            self::$_log->debug('BasePath: '.$this->_basePath);
+        }
+    }
+
+    function process(Request $req)
+    {
+        self::$_log->debug(__METHOD__.' called');
+        self::$_log->debug('Requested URL: '.$req->url);
+        $truePath = $this->_basePath.strtr(
+            substr($req->url, strpos($req->url,'/',1)),
+            PATH_SEPARATOR,
+            DIRECTORY_SEPARATOR);
+        $resp = $req->respond('');
+        self::$_log->debug('Serving file: '.$truePath);
+        if (file_exists($truePath)) {
+            $returnCode = 0;
+            $resp->messageBody = fopen($truePath, 'rb');
+            $resp->send();
+        } else {
+            $returnCode = 1;
+            $resp->messageBody = 'File not found';
+            $resp->statusCode = 404;
+            $resp->send();
+        }
+        return $returnCode;
+    }
+}
