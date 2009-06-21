bfdf2dd75c1c7ec9a78187ab75ea0b2001b076ea
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-06-21 18:21:07 +0000

    + Added multiple engine handling
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@275 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Server/AppServer.class.php
+++ Server/AppServer.class.php
@@ -26,15 +26,17 @@ class AppServer extends Server
     protected $_appID = '';
     protected $_pidFolder = '.';
     protected $_appReg = null;
-    protected $_engine = null;
+    protected $_engines = array();
     private $_includes = array();
     private $_address = null;
     private $_port = null;
     private $_socket = null;
     private $_accepting = false;
+    private $_urimap = array();
 
     const DEFAULT_ADDRESS = '127.0.0.1';
     const DEFAULT_PORT = 8085;
+    const DEFAULT_TIMEOUT = 90;
 
     /**
      * Constructor method fo initializing with a Config object
@@ -50,14 +52,19 @@ class AppServer extends Server
         self::$_log->debug('AppID: '.$this->_appID);
         $this->_pidFileName = sprintf('.%s_srphp.pid', $this->_appID);
         self::$_log->debug('PidFile name: '.$this->_pidFileName);
-        self::$_log->debug('Including files');
-        //Requireing all the files which are in the Config xml
+        self::$_log->debug('Adding include pathes');
+        //Adding all pathes listed in config "includes/path"
+        //They will not be added to include path before daemon is
+        //summoned to lock out other processes seeing those pathes
         if (isset($conf->includes)) {
             foreach ($conf->includes as $key => $resource) {
-                self::$_log->debug('Requireing '.$resource);
-                if (require_once $resource) {
-                    self::$_log->debug($resource. 'included');
+                self::$_log->debug('path: '.$resource);
+                if (is_dir($resource)) {
                     array_push($this->_includes, $resource);
+                    self::$_log->debug($resource. 'added');
+                } else {
+                    throw new Exception($resource.
+                        ' is not a directory to include');
                 }
             }
         }
@@ -71,17 +78,25 @@ class AppServer extends Server
             } else {
                 parent::__construct();
             }
-            //Setting up server engine
-            if (isset($instance->engine)) {
-                self::$_log->debug('Initalizing engine: '.$instance->engine);
-                //Class should be already "required-in" above
-                $this->_engine = new $engine;
+           self::$_log->debug('Setting up server engines');
+            if (isset($instance->engines)) {
+                foreach ($instance->engines->children() as $engine) {
+                    $this->_engines[(string)$engine['id']] = $engine;
+                }
             } else {
-                self::$_log->debug('Initalizing default engine');
-                require_once 'Server/DefaultEngine.class.php';
-                $this->_engine = new DefaultEngine;
+                self::$_log->debug('Initalizing default engines');
+                $conf = '<engine id="default" class="Default" />';
+                $this->_engines['default'] = new Config($conf);
             }
-            //Setting up socket address and port
+            self::$_log->debug('Setting up URImaps');
+            if (isset($conf->urimap)) {
+                $this->_urimap = $conf->urimap;
+            } else {
+                $this->_urimap = new Config('<urimap />');
+                $this->_urimap->url = '/';
+                $this->_urimap->url['engine'] = 'default';
+            }
+            self::$_log->debug('Setting up socket address, port and timeout');
             if (isset($instance->address)) {
                 $this->_address = (string) $instance->address;
             } else {
@@ -92,8 +107,16 @@ class AppServer extends Server
             } else {
                 $this->_port = self::DEFAULT_PORT;
             }
-            self::$_log->debug('Using '.
-                sprintf('%s:%d',$this->_address, $this->_port));
+            if (isset($instance->timeout)) {
+                $this->_timeout = (integer) $instance->timeout;
+            } else {
+                $this->_timeout = self::DEFAULT_TIMEOUT;
+            }
+            self::$_log->info('Using '.
+                sprintf('%s:%d w/ %d sec timeout',
+                    $this->_address,
+                    $this->_port,
+                    $this->_timeout));
         }
         self::$_log->debug('Setting up Application registry');
         if ($this->_ipcType !== '') {
@@ -113,8 +136,46 @@ class AppServer extends Server
         if ($this->_ipcType !== '') {
             $this->_appReg->useIpc($this->_ipc);
         }
+        $this->_setupIncludePathes();
+        $this->_initEngines();
         self::$_log->debug('Initalizing socket listening on');
         $this->initSocket();
+    }
+
+    /**
+     * Sets up process global include pathes
+     *
+     * Adds pathes from $this->_includes to include_path, if it is not already
+     * in it.
+     *
+     * @return void
+     */
+    protected function _setupIncludePathes()
+    {
+        foreach ($this->_includes as $path) {
+            $currIncludePath = get_include_path();
+            if (strpos($currIncludePath,$path) === false) {
+                set_include_path($currIncludePath . PATH_SEPARATOR . $path);
+            }
+        }
+    }
+
+    /**
+     * Instantiates registered server engines
+     *
+     * Tryes to require every file stored in $this->_engines and if succeds
+     * creates and instance of it and stores in place of the class name in
+     * $this->_engines.
+     *
+     * @return void
+     */
+    protected function _initEngines()
+    {
+        foreach ($this->_engines as $name=>$conf) {
+            $class = $conf['class'].'Engine';
+            require_once $class.'.class.php';
+            $this->_engines[$name] = new $class($conf);
+        }
     }
 
     /**
@@ -158,20 +219,23 @@ class AppServer extends Server
     {
         //Function usually called every 200 microsec
         if ($conn = @stream_socket_accept($this->_socket)) {
-            self::$_log->debug(stream_get_meta_data($conn));
             self::$_log->debug('Connection accepted, spawning new child');
             $this->spawn();
             if ($this->_role == 'child') {//we are the new process
                 $this->_accepting = false;
-                //stream_socket_shutdown($this->_socket, STREAM_SHUT_RD);
                 stream_set_blocking($conn, 0);
                 try {
-                    $result = $this->process(RequestFactory::create($conn));
-                }catch (Exception $e) {
+                    $result = $this->process(RequestFactory::create($conn),
+                        $this->_timeout);
+                } catch (IOException $e) {
+                    stream_socket_shutdown($conn, STREAM_SHUT_RDWR);
+                    self::$_log->alert('Error: '.$e->getMessage());
+                    $result = 0;
+                } catch (Exception $e) {
                     stream_socket_sendto($conn,
                         'HTTP/1.0 500 Internal Server Error');
                     self::$_log->alert('Error: '.$e->getMessage());
-                    $result = 500;
+                    $result = 1;
                 }
                 stream_socket_shutdown($conn, STREAM_SHUT_RDWR);
                 exit($result);
@@ -213,6 +277,8 @@ class AppServer extends Server
     /**
      * Start the processing of the received Request.
      *
+     * Selects the apropriate server engine to handle the request. If there is
+     * no such (not registered or not loaded) it will generate 404 message.
      * Method should return with an integer, which will be the exit
      * status of the child process doing the processing.
      *
@@ -222,7 +288,35 @@ class AppServer extends Server
     public function process(Request $req)
     {
         self::$_log->debug(__METHOD__.' called');
-        return $this->_engine->process($req);
+        $path = parse_url($req->url,PHP_URL_PATH);
+        $engines = $this->_urimap->children();
+        $engine = false;
+        foreach ($engines as $node=>$value) {
+            if (strpos($path, (string)$value) !== false) {
+                $engine = (string)$value['engine'];
+                continue;
+            }
+        }
+        self::$_log->debug("Engine: ".$engine);
+        if ($engine === false) {
+            $returnCode = 404;
+            self::$_log->debug("Not found: ".$engine);
+            $response = $req->respond('File not found!',
+                array('statusCode'=>$returnCode));
+            $response->send();
+        } else {
+            if (array_key_exists($engine, $this->_engines)) {
+                self::$_log->debug("Processing w/".$engine);
+                $returnCode = $this->_engines[$engine]->process($req);
+            }else{
+                $returnCode = 404;
+                self::$_log->debug("Not registered: ".$engine);
+                $response = $req->respond('File not found!',
+                    array('statusCode'=>$returnCode));
+                $response->send();
+            }
+        }
+        return $returnCode;
     }
 
     /* (non-PHPdoc)
