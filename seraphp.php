93c64be7c3d32ed4107d0afd2446ea9fe033f649
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-06-28 21:43:06 +0000

    Added comand line parameter handling
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@297 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- seraphp.php
+++ seraphp.php
@@ -4,11 +4,30 @@
  * Example startup file for Seraphp server
  * @package Seraphp
  */
-require_once 'Exceptions/ExceptionHandler.class.php';
-ExceptionHandler::setup();
-require_once 'Server/AppServer.class.php';
-require_once 'Server/Config/ConfigFactory.class.php';
+require_once 'Server/ServerManager.class.php';
 $cf = ConfigFactory::getInstance();
-$cf->setXmlSrc('./seraphpConf.xml');
-$mainServer = new AppServer($cf->getConf('main'));
-$procID = $mainServer->summon();
+if ($argc > 1) {
+    if (isset($argv[2])) {
+        $server = $argv[2];
+    } else {
+        $server = 'main';
+    }
+    if (isset($argv[3])) {
+        $cf->setXmlSrc($argv[3]);
+    } else {
+        $cf->setXmlSrc('./seraphpConf.xml');
+    }
+    switch ($argv[1]) {
+        case 'start':
+            ServerManager::startup($server);
+            break;
+        case 'stop':
+            ServerManager::shutdown($server);
+            break;
+        case 'restart':
+            ServerManager::restart($server);
+            break;
+        default: echo '1st paramter has to be the required action: start|stop|restart';
+    }
+} else {
+}
