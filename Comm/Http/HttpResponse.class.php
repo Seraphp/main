0cf5968afe5b225fbe4e2a5ed9c116acae9dd9df
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2009-08-09 17:21:15 +0000

    Refactored send() to lower CRAP Index from 110
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/seraphp/trunk@334 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Comm/Http/HttpResponse.class.php
+++ Comm/Http/HttpResponse.class.php
@@ -55,60 +55,68 @@ class HttpResponse
     {
         self::$_log->debug(__METHOD__.' called');
         if ($this->toBeSend) {
-            $this->headers['Date'] = date(DATE_RFC1123);
-            //Keep-Alive feature not implemented yet
-            $this->headers['Connection'] = 'Closed';
             //Creating Status line
             $this->statusLine = sprintf('HTTP/%s %d %s',
-                                 $this->httpVersion,
-                                 $this->statusCode,
-                                 HttpFactory::getHttpStatus($this->statusCode));
+                             $this->httpVersion,
+                             $this->statusCode,
+                             HttpFactory::getHttpStatus($this->statusCode));
             $buffer = $this->statusLine."\r\n";
-            //If a file is to be sent in the message bdy
-            if (is_resource($this->messageBody)) {
-                $data = array_merge(fstat($this->messageBody),
-                    stream_get_meta_data($this->messageBody));
-                $this->headers['Content-Length'] = $data['size'];
-                $this->headers['Last-Modified'] =
-                    date(DATE_RFC1123, $data['mtime']);
-                $this->headers['Etag'] = md5_file($data['uri']);
-            } else {
-                $this->headers['Content-Length'] = strlen($this->messageBody);
-                $this->headers['Etag'] = md5($this->messageBody);
-            }
-            if ($this->headers !== array()) {
-                $buffer .= $this->_headers()."\r\n";
-            }
+            $buffer .= $this->_getHeaders()."\r\n";
             self::$_log->debug('Sending out head part');
             if (fwrite($this->_socket, $buffer."\r\n") === false) {
                 throw new IOException('Cannot send out header part');
             }
             if (!empty($this->messageBody)) {
-                self::$_log->debug('Sending message body');
-                if (is_resource($this->messageBody)) {
-                    if (stream_copy_to_stream($this->messageBody,
-                        $this->_socket) === false) {
-                        throw new IOException('Cannot send message body!');
-                    }
-                    if (fwrite($this->_socket, "\r\n") === false) {
-                        throw new IOException('Cannot send message body!');
-                    }
-                } else {
-                    if (fwrite($this->_socket,
-                        $this->messageBody."\r\n") === false) {
-                        throw new IOException('Cannot send message body!');
-                    }
-                }
+                $this->_sendBody();
             }
             stream_socket_shutdown($this->_socket, STREAM_SHUT_RDWR);
         }
     }
 
-    private function _headers()
+    private function _configHeaders()
     {
-        $headers = array();
+        $this->headers['Date'] = date(DATE_RFC1123);
+        //Keep-Alive feature not implemented yet
+        $this->headers['Connection'] = 'Closed';
+        //If a file is to be sent in the message bdy
+        if (is_resource($this->messageBody)) {
+            $data = array_merge(fstat($this->messageBody),
+                stream_get_meta_data($this->messageBody));
+            $this->headers['Content-Length'] = $data['size'];
+            $this->headers['Last-Modified'] =
+                date(DATE_RFC1123, $data['mtime']);
+            $this->headers['Etag'] = md5_file($data['uri']);
+        } else {
+            $this->headers['Content-Length'] = strlen($this->messageBody);
+            $this->headers['Etag'] = md5($this->messageBody);
+        }
+    }
+
+    private function _sendBody()
+    {
+        self::$_log->debug('Sending message body');
+        if (is_resource($this->messageBody)) {
+            if (stream_copy_to_stream($this->messageBody,
+                $this->_socket) === false) {
+                throw new IOException('Cannot send message body!');
+            }
+            if (fwrite($this->_socket, "\r\n") === false) {
+                throw new IOException('Cannot send message body!');
+            }
+        } else {
+            if (fwrite($this->_socket,
+                $this->messageBody."\r\n") === false) {
+                throw new IOException('Cannot send message body!');
+            }
+        }
+    }
+
+    private function _getHeaders()
+    {
         self::$_log->debug(__METHOD__.' called');
         self::$_log->debug('Creating header lines');
+        $headers = array();
+        $this->_configHeaders();
         foreach ($this->headers as $key=>$value) {
             self::$_log->debug(sprintf('%s:%s', $key, $value));
             array_push($headers, sprintf('%s:%s', $key, $value));
