ee4f7df788571ecdb439b00e5567bab0c8b4410c
peter <peter@5e58f187-e6ed-c349-b56f-493b30cdff1d>
2010-01-30 15:43:47 +0000

    - Comments added - streamlined shutdown process
    git-svn-id: https://pl3.projectlocker.com/Tronic/Seraphp/svn/trunk@423 5e58f187-e6ed-c349-b56f-493b30cdff1d

--- Server/ServerManager.class.php
+++ Server/ServerManager.class.php
@@ -29,6 +29,13 @@ class ServerManager
     {
     }
 
+    /**
+     * Starts a server instance
+     *
+     * @param string $appID
+     * @return void
+     * @static
+     */
     static function startup($appID='main')
     {
         self::_init();
@@ -40,12 +47,32 @@ class ServerManager
         self::$_reg->storePid($appID, $pid);
     }
 
+    /**
+     * Restarts a running server instance
+     *
+     * @param string $appID
+     * @return void
+     * @static
+     */
     static function restart($appID)
     {
         self::_init();
         $currStatus = self::$_reg->getAppStatus($appID);
         if ($currStatus === 'running') {
             $oldProcess = self::$_reg->getAppInstance($appID);
+            self::write('Shuting down old: '.$appID);
+            $result = $oldProcess->expel();
+            if ($result == true) {
+                posix_kill(self::$_reg->getPid($appID), SIGTERM);                self::writeln('...OK');
+                self::$_reg->removeApp($appID);
+                self::writeln('...OK');
+            } else {
+                self::writeln('...Failed');
+            }
+            if ($newProcess === true) {
+                self::$_reg->addApp($appID, $newProcess);
+                self::$_reg->storePid($appID, $newProcess->summon());
+            }
             self::write('Starting up new server: '.$appID);
             $newProcess = AppServerFactory::getAppInstance(
                 $appID, self::$_cf->getConf($appID)
@@ -55,23 +82,18 @@ class ServerManager
             } else {
                 self::writeln('...Failed');
             }
-            self::write('Shuting down old: '.$appID);
-            $result = $oldProcess->expell();
-            if ($result === true) {
-                self::writeln('...OK');
-                self::$_reg->removeApp($appID);
-            } else {
-                self::writeln('...Failed');
-            }
-            if ($newProcess === true) {
-                self::$_reg->addApp($appID, $newProcess);
-                self::$_reg->storePid($appID, $newProcess->summon());
-            }
         } else {
             self::writeln($appID.' is '.$currStatus);
         }
     }
 
+    /**
+     * Close down a runing server instance
+     *
+     * @param string $appID
+     * @return void
+     * @static
+     */
     static function shutdown($appID)
     {
         self::_init();
@@ -79,8 +101,9 @@ class ServerManager
         if ($currStatus == 'running') {
             $process = self::$_reg->getAppInstance($appID);
             self::write('Shuting down '.$appID);
-            $result = $process->expell();
-            if ($result === true) {
+            $result = $process->expel();
+            if ($result == true) {
+                posix_kill(self::$_reg->getPid($appID), SIGTERM);
                 self::writeln('...OK');
                 self::$_reg->removeApp($appID);
             } else {
@@ -91,18 +114,38 @@ class ServerManager
         }
     }
 
+    /**
+     * Write a message to std. output
+     *
+     * @param string $message
+     * @return void
+     * @static
+     */
     static function write($message)
     {
         echo ($message);
         self::$_log->debug($message);
     }
 
+    /**
+     * Write a message to std. output with new line at the end
+     *
+     * @param string $message
+     * @return void
+     * @static
+     */
     static function writeln($message)
     {
         self::write($message);
         echo "\n";
     }
 
+    /**
+     * Initialize system beofre accepting any command
+     *
+     * @return void
+     * @static
+     */
     private static function _init()
     {
         ExceptionHandler::setup();
